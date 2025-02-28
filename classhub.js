8f21f8b326f40640f917df06cb28c52b4ceb4a78
proplayer919 <95329795+proplayer919@users.noreply.github.com>
2025-03-01 09:35:12 +1100

    make it even more on top

--- classhub.js
+++ classhub.js
@@ -69,7 +69,7 @@
       font-family: Arial, sans-serif;
       background-color: var(--window-bg);
       border: 1px solid var(--border-color);
-      z-index: 9999;
+      z-index: 999999999999999999999999999999999999999999999999999999999999999999999999;
     }
     .console-header {
       background-color: var(--header-bg);
